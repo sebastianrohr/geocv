5d905aeb332d4a990b321faadff42fe32185b189
juthoma22 <julius9819@hotmail.de>
2023-04-20 15:59:48 +0200

    kaggle download and unzip into './output'

--- src/kaggle_download.py
+++ src/kaggle_download.py
@@ -1,26 +1,18 @@
 import os
 import zipfile
-from io import BytesIO
-from urllib.request import urlopen
+from kaggle.api.kaggle_api_extended import KaggleApi
 
-# To get API key, go in kaggle to 'Your Profile' -> 'Account' -> 'Create New API Token'
-# then place the file in C:\Users\username\.kaggle
+# set the API key for authentication
+api = KaggleApi()
+api.authenticate()
 
-# If error 401 unathorized hapens, redownload a new api key and do the above steps again
+# set the dataset name and download path
+user = "sebastianrohr"
+dataset_name = 'geocv-dataset'
+download_url = user + '/' + dataset_name
+download_path = 'output'
 
+# download the dataset
+api.dataset_download_files(download_url, path=download_path, quiet=False, force=True, unzip=True)
 
-# Download the dataset zip file
-os.system("kaggle datasets download -d sebastianrohr/geocv-dataset -p {os.getcwd()}")
-
-# Extract the images from the zip file and stream them to your project
-for filename in os.listdir(os.getcwd()):
-    if filename.endswith('.zip'):
-        with zipfile.ZipFile(filename, 'r') as zip_ref:
-            for folder in ['copenhagen', 'madrid', 'london', 'moscow']:
-                for file in zip_ref.namelist():
-                    if file.startswith(folder) and file.endswith(".jpg"):
-                        img_data = zip_ref.read(file)
-                        # Here, you can use the img_data variable to stream the image data to your machine learning project.
-                        # For example, if you're using OpenCV to process the images, you can convert the image data to a NumPy array:
-                        # import cv2
-                        # img_np = cv2.imdecode(np.frombuffer(img_data, np.uint8), cv2.IMREAD_COLOR)
+print(f"{dataset_name} has been downloaded and unzipped successfully!")
